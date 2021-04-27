f5a830f5a83304331c93e7ea343a76c68a40ad1e
mrom <mrom@linuxmail.org>
2021-04-27 18:37:08 +0200

    update workflow/cmake linux/windows

--- cmake/FindBlake2.cmake
+++ cmake/FindBlake2.cmake
@@ -1,25 +1,24 @@
 include(ExternalProject)
 
 SET(BLAKE2_SEARCH_PATH
-    "${CMAKE_MODULE_PATH}/../extern/libb2")
+    "${PROJECT_SOURCE_DIR}/extern/libb2")
 
 if(WIN32)
     ExternalProject_Add(
-        libb2
+        libb2_SRC
         GIT_REPOSITORY https://github.com/mrom1/libb2
         GIT_TAG feature/cmake
         UPDATE_COMMAND ""
         PREFIX ${BLAKE2_SEARCH_PATH}
         SOURCE_DIR "${BLAKE2_SEARCH_PATH}/src/libb2"
         BUILD_IN_SOURCE 1
-
         INSTALL_COMMAND ""
     )
 endif(WIN32)
 
 if(UNIX)
     ExternalProject_Add(
-        libb2
+        libb2_SRC
         GIT_REPOSITORY https://github.com/BLAKE2/libb2.git
         UPDATE_COMMAND ""
         PREFIX ${BLAKE2_SEARCH_PATH}
@@ -31,10 +30,12 @@ if(UNIX)
     )
 endif(UNIX)
 
-ExternalProject_Get_Property(libb2 source_dir)
+set(LIBSUFFIX "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+ExternalProject_Get_Property(libb2_SRC source_dir)
 set(BLAKE2_INCLUDES "${source_dir}/src")
     
-ExternalProject_Get_Property(libb2 binary_dir)
+ExternalProject_Get_Property(libb2_SRC binary_dir)
 
 if(WIN32)
     set(BLAKE2_LIBRARY ${binary_dir}/lib/Debug/${CMAKE_FIND_LIBRARY_PREFIXES}libb2${LIBSUFFIX})
@@ -44,6 +45,6 @@ if(UNIX)
     set(BLAKE2_LIBRARY ${binary_dir}/src/.libs/${CMAKE_FIND_LIBRARY_PREFIXES}b2${LIBSUFFIX})
 endif(UNIX)
 
-set_target_properties(libb2 PROPERTIES
-    IMPORTED_LOCATION                 "${BLAKE2_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES     "${BLAKE2_INCLUDES}")
+add_library(libb2 SHARED IMPORTED)
+
+set_target_properties(libb2 PROPERTIES IMPORTED_LOCATION "${BLAKE2_LIBRARY}")
